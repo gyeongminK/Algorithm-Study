e79f7a6e0965d0f426f283b18549adaafd714625
Gyeongmin Kim <66426083+gyeongminK@users.noreply.github.com>
2022-06-20 13:06:51 +0900

    Create 23. 국영수.py

---
+++ 14. 정렬 문제/23. 국영수.py
@@ -0,0 +1,12 @@
+# 23. 국영수
+
+n = int(input())
+students = []
+for _ in range(n):
+	name, korean, english, math = map(str, input().split())
+	students.append([name, int(korean), int(english), int(math)])
+
+students.sort(key= lambda x: (-x[1], x[2], -x[3], x[0]))
+
+for i in range(n):
+	print(students[i][0])
